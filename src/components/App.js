b854352bbb601f2fa60d58656a94548960c8585a
Alejandro Toledo <atoledofr@gmail.com>
2021-05-24 14:56:48 -0500

    add react-router-dom and create navbar, devices and new device file

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import '../styles/App.css';
+import Navbar from './navbar';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
        <h1 className="text-white mt-3 mb-3 pt-3 pb-3">Ninja RMM Front End Task</h1>
        <h3 className="text-white mt-2 mb-2 pt-2 pb-3">Devices Task</h3>
       </header>
+      <Navbar />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './styles/index.css';
+import App from './components/App';
+import Devices from './components/Devices';
+import NewDevice from './components/NewDevice';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    <Switch>
+      <Route exact path="/" component={Devices} />
+      <Route to="/newdevice" component={NewDevice} />
+    </Switch>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
