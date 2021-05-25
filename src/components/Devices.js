91c484de1b5660f1a43b4deeca736f702bed516f
Alejandro Toledo <atoledofr@gmail.com>
2021-05-25 10:02:09 -0500

    add deleteDevice method and getDevices method from useEffect

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-// import Filter from './Filter';
 
 const Devices = () => {
   const [devices, setDevices] = useState([]);
@@ -7,13 +6,17 @@ const Devices = () => {
   const [filter, setFilter] = useState("");
 
 useEffect(() => {
+  getDevices()
+}, [])
+
+const getDevices = () => {
   fetch("http://localhost:3000/devices")
   .then(response => response.json())
   .then(data => {
     setDevices(data)
     setLoading(false)
   });
-}, [])
+}
 
 if (loading) {
   return <p className="text-center">Loading Devices...</p>
@@ -22,6 +25,16 @@ if (loading) {
 const filteredDevices = devices.filter(devices => {
   return devices.type.includes(filter)
 })
+
+const deleteDevice = (id) => {
+  fetch(`http://localhost:3000/devices/${id}`,{
+    method: "DELETE",
+  }).then(response => response.json()
+    .then((data) => {
+      console.warn(data)
+      getDevices()
+    }))
+}
 
 
 // handleChangeSort(e){
@@ -47,14 +60,18 @@ const filteredDevices = devices.filter(devices => {
         <option value="">All</option>
         <option value="WINDOWS_WORKSTATION">Windows Workstation</option>
         <option value="WINDOWS_SERVER">Windows Server</option>
-        <option value="Mac">Mac</option>
+        <option value="MAC">Mac</option>
       </select>
-      {filteredDevices.map(device => (
-        <div className="border rounded mt-3 mb-3">
-          <div key={device.id} className="container mt-3">
+      {filteredDevices.map((device, i) => (
+        <div key={i} className="border rounded container mt-3 mb-3 d-flex justify-content-around">
+          <div className="mt-3 mr-4 pr-4">
             <p className="pl-2"><strong>Device System Name:</strong> {device.system_name}</p>
             <p className="ml-3"><strong>Device Type:</strong> {device.type}</p>
             <p className="pl-2"><strong>HDD Capacity:</strong> {device.hdd_capacity} gb</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <button className="btn btn-secondary">Edit Device</button>
+            <button className="btn btn-danger" onClick={() => deleteDevice(device.id)}>Delete device</button>
           </div>
         </div>
       ))}
