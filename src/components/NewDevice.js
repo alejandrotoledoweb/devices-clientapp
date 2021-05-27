47d589c896f9962097c69e218e92026a73d49f04
alejandrotoledoweb <atoledofr@gmail.com>
2021-05-27 12:38:01 -0500

    add style to form to create new device

--- src/components/NewDevice.js
+++ src/components/NewDevice.js
@@ -43,12 +43,13 @@ const Devices = () => {
   };
 
   return (
-    <div className="">
+    <div className="mb-5 container">
+      <div className="col-lg-6 col-md-6 col-sm-6 d-flex flex-column">
       <h3 className="mt-3 mb-4">Enter the following information to create a new device</h3>
       <p className="mt-2">Enter the name of the device</p>
       <input className="mb-2 mt-2 rounded" id="input1" onChange={e => setSystem(e.target.value.toUpperCase())} placeholder="Alex Device" type="text" required/> <br></br>
       <p className="mt-2">Select the device type</p>
-      <select className="w-25 mb-3" id="input2" onChange={e => setType(e.target.value)} required >
+      <select className="w-100 mb-3" id="input2" onChange={e => setType(e.target.value)} required >
         <option value="">Select</option>
         <option value="WINDOWS_WORKSTATION">WINDOWS WORKSTATION</option>
         <option value="WINDOWS_SERVER">WINDOWS SERVER</option>
@@ -57,6 +58,8 @@ const Devices = () => {
       <p>Enter the disk capacity in GB</p>
       <input className="mb-3 rounded" id="input3" onChange={e => setCapacity(e.target.value)} type="number" placeholder="512" required /><br></br>
       <button className="btn btn-primary" onClick={addDevice}>Create new device</button>
+      </div>
+      
 
       <div>
       <Modal show={show} onHide={handleClose}>
