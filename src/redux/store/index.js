6e5c6a57428d4a136a83de800943dde30dfc7cfd
alejandrotoledoweb <atoledofr@gmail.com>
2021-06-11 14:53:51 -0500

    setup the store for redux app with middleware

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,1 +1,8 @@
-import 
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+const composeEnahncer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const myStore = createStore(composeEnahncer(applyMiddleware(thunk)));
+
+export default myStore;
