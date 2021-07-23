a11e278010b8a1cc549bc07ea081d46f0c50a9d6
alejandrotoledoweb <atoledofr@gmail.com>
2021-07-23 16:20:42 -0500

    fix filter and sort default value in component FilterSort.js

--- src/components/FilterSort.js
+++ src/components/FilterSort.js
@@ -7,14 +7,14 @@ const FilterSort = ({ handleFilter, handleSort, count }) => {
     <>
       <div></div>
       <select className="mt-1 mb-2 mr-2 pr-2" onChange={handleFilter}>
-        <option value="">Device Type: All</option>
+        <option defaultValue>Filter: All</option>
         <option value="WINDOWS_WORKSTATION">Device Type: Windows Workstation</option>
         <option value="WINDOWS_SERVER">Device Type: Windows Server</option>
         <option value="MAC">Device Type: Mac</option>
       </select>
       <div className="mr-1"></div>
       <select className="mt-1 mb-2 ml-2" onChange={handleSort} >
-        <option value="">Sort by: All</option>
+        <option defaultValue>Sort by: All</option>
         <option value="system">Sort by: System Name</option>
         <option value="hdd">Sort by: HDD Capacity</option>
       </select>
