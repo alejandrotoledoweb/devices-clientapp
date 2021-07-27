df5b7cb6c20885190aaad34c200f4735bc67556e
alejandrotoledoweb <atoledofr@gmail.com>
2021-07-27 14:24:00 -0500

    delete count from filterSort component

--- src/components/FilterSort.js
+++ src/components/FilterSort.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FilterSort = ({ handleFilter, handleSort, count }) => {
+const FilterSort = ({ handleFilter, handleSort }) => {
 
   return (
     <>
