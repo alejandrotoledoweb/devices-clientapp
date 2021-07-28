4eb61b4393ae2739e2d07d2d4c95fbd52bd85797
alejandrotoledoweb <atoledofr@gmail.com>
2021-07-28 15:49:36 -0500

    add action types for filter and sort functions

--- src/redux/constants/actionTypes.js
+++ src/redux/constants/actionTypes.js
@@ -8,7 +8,8 @@ export const ActionTypes = {
   EDIT_DEVICE: "EDIT_DEVICE",
   FILTER_DEVICES: "FILTER_DEVICES",
   SET_FILTER: "SET_FILTER",
-  SET_SORT: "SET_SORT"
+  SET_SORT: "SET_SORT",
+
 };
 
 export default ActionTypes;
